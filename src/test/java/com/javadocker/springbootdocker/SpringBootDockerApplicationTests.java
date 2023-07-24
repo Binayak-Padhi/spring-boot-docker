00891e2a628532f5c4b479aa5f8e503339f0fb97
binay <binay@LAPTOP-KDMEBVTJ>
2023-07-24 11:11:23 +0530

    First commit

---
+++ src/test/java/com/javadocker/springbootdocker/SpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadocker.springbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
